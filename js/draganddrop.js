63df20308c4e07cb79c6c9c349f4730a5c178e15
Dmitry <likesatie@gmail.com>
2018-10-06 12:22:42 +0300

    drag and drop behaviour changed

--- js/draganddrop.js
+++ js/draganddrop.js
@@ -1,112 +1,43 @@
-let movedPiece = null;
-let minY;
-let minX;
-let maxX;
-let maxY;
-let shiftX = 0;
-let shiftY = 0;
+"use strict";
 
-dragElement.addEventListener("mousedown", dragStart, false);
+document.body.addEventListener("dragover", event => event.preventDefault());
+document.body.addEventListener("drop", onFilesDrop);
+document.addEventListener("mousemove", dragStart, false);
+let movedPiece = null;
+
+document.addEventListener("mouseup", () => {
+  if (movedPiece) {
+    movedPiece = null;
+  }
+});
 
 function dragStart(event) {
-  if (event.which !== 1) {
-    return;
-  }
-
-  movedPiece = menu;
-  minY = wrap.offsetTop + 1;
-  minX = wrap.offsetLeft + 1;
-  maxX = wrap.offsetLeft + wrap.offsetWidth - movedPiece.offsetWidth - 1;
-  maxY = wrap.offsetTop + wrap.offsetHeight - movedPiece.offsetHeight - 1;
-  shiftX =
-    event.pageX -
-    event.target.getBoundingClientRect().left -
-    window.pageXOffset;
-  shiftY =
-    event.pageY - event.target.getBoundingClientRect().top - window.pageYOffset;
-}
-
-document.addEventListener("mousemove", event => drag(event.pageX, event.pageY));
-
-function drag(x, y) {
-  if (wrap.offsetWidth === menu.offsetWidth + parseInt(menu.style.left) + 1) {
-    drop();
-    menu.style.left = parseInt(menu.style.left) - 2 + "px";
-  } else if (menu.style.left === "1px") {
-    drop();
-    menu.style.left = parseInt(menu.style.left) + 2 + "px";
-  } else if (menu.style.top === "1px") {
-    drop();
-    menu.style.top = parseInt(menu.style.top) + 2 + "px";
-  } else if (
-    wrap.offsetHeight ===
-    menu.offsetHeight + parseInt(menu.style.top) + 1
-  ) {
-    drop();
-    menu.style.top = parseInt(menu.style.top) - 2 + "px";
-  }
-
   if (movedPiece) {
-    x = x - shiftX;
-    y = y - shiftY;
-    x = Math.min(x, maxX);
-    y = Math.min(y, maxY);
-    x = Math.max(x, minX);
-    y = Math.max(y, minY);
-    movedPiece.style.left = x + "px";
-    movedPiece.style.top = y + "px";
-  } else {
-    return;
+    event.preventDefault();
+    const cords = {
+      x: event.pageX - shiftMenu.x,
+      y: event.pageY - shiftMenu.y
+    };
+    const maxX = pageDimension.width - movedPiece.offsetWidth - 1;
+    const maxY = pageDimension.height - movedPiece.offsetHeight - 1;
+    cords.x = Math.min(cords.x, maxX);
+    cords.y = Math.min(cords.y, maxY);
+    cords.x = Math.max(cords.x, 0);
+    cords.y = Math.max(cords.y, 0);
+    movedPiece.style.left = `${cords.x}px`;
+    movedPiece.style.top = `${cords.y}px`;
   }
 }
 
-dragElement.addEventListener("mouseup", drop, false);
-
-function drop() {
-  movedPiece = null;
-}
-
-bodyEl.addEventListener("dragover", dragOverHandler, false);
-
-function dragOverHandler(event) {
+function onFilesDrop(event) {
+  console.log(`Файл выбран. Функция onFilesDrop()`);
   event.preventDefault();
-}
-
-bodyEl.addEventListener("dragend", dragEndHandler, false);
-
-function dragEndHandler(event) {
-  const data = event.dataTransfer;
-  if (data.items) {
-    for (let i = 0; i < data.items.length; i++) {
-      data.items.remove(i);
-    }
+  if (!img.getAttribute("src")) {
+    const files = event.dataTransfer.files;
+    sendFile(files[0]);
   } else {
-    event.dataTransfer.clearData();
-  }
-}
-
-bodyEl.addEventListener("drop", dropHandler, false);
-
-function dropHandler(event) {
-  event.preventDefault();
-  const data = event.dataTransfer;
-
-  if (
-    data.items[0].kind === "file" &&
-    (data.items[0].type === "image/png" || data.items[0].type === "image/jpeg")
-  ) {
-    const dropDateItem = data.items[0].getAsFile();
-    formatError.classList.add("hidden");
-    checkForReDownloadThroughDrop(dropDateItem);
-  } else {
-    formatError.classList.remove("hidden");
-  }
-}
-
-function checkForReDownloadThroughDrop(dropDateItem) {
-  if (image.getAttribute("src") === "./pic/image.png") {
-    sendImage(dropDateItem);
-  } else {
-    repeatDownloadErr.classList.remove("hidden");
+    errorWrap.classList.remove("hidden");
+    errorMessage.innerText =
+      'Чтобы загрузить изображение, воспользуйтесь пунктом "Загрузить новое" в меню.';
   }
 }
