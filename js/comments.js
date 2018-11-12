93e55589ccebf03cfaf292dae603f3de4f042766
likesatie@gmail.com <likesatie@gmail.com>
2018-11-12 20:17:52 +0300

    A_06

--- js/comments.js
+++ js/comments.js
@@ -40,11 +40,73 @@ function closeAllForms() {
   }
 }
 
-function closeAllForms() {
-  //   console.log(`Запущена функция closeAllForms()`);
-  const otherForms = document.querySelectorAll('.comments__body');
-  for (const body of otherForms) {
-    body.style.display = 'none';
+function createNewComment(event) {
+  console.log(event.target);
+  console.log(event.currentTarget);
+  console.log(event.pageX, event.pageY);
+  if (event.target != formContainer) {
+    return;
+  }
+  const isCommentsOn = document.getElementById('comments-on').checked;
+  if (comments.dataset.state === 'selected' && isCommentsOn) {
+    const app = document.querySelector('.app');
+    removeEmptyComment();
+    closeAllForms();
+
+    const form = document.createElement('div');
+    form.className = 'comments__form new';
+
+    const marker = document.createElement('span');
+    marker.className = 'comments__marker';
+
+    const commentsBody = document.createElement('div');
+    commentsBody.className = 'comments__body';
+
+    const createMessaege = document.createElement('div');
+    createMessaege.className = 'comment';
+
+    const loader = document.createElement('div');
+    loader.className = 'loader hidden';
+
+    const span = document.createElement('span');
+
+    const commentsInput = document.createElement('textarea');
+    commentsInput.className = 'comments__input';
+    commentsInput.setAttribute('type', 'text');
+    commentsInput.setAttribute('placeholder', 'Напишите ответ...');
+
+    const commentsClose = document.createElement('input');
+    commentsClose.className = 'comments__close';
+    commentsClose.type = 'button';
+    commentsClose.value = 'Закрыть';
+
+    const commentsSubmit = document.createElement('input');
+    commentsSubmit.className = 'comments__submit';
+    commentsSubmit.type = 'submit';
+    commentsSubmit.value = 'Отправить';
+
+    createMessaege.appendChild(loader);
+    loader.appendChild(span);
+    loader.appendChild(span);
+    loader.appendChild(span);
+    loader.appendChild(span);
+    loader.appendChild(span);
+    commentsBody.appendChild(createMessaege);
+    commentsBody.appendChild(commentsInput);
+    commentsBody.appendChild(commentsClose);
+    commentsBody.appendChild(commentsSubmit);
+
+    form.style.left =
+      event.pageX - (image.offsetLeft - image.offsetWidth / 2) + 'px';
+    form.style.top =
+      event.pageY - (image.offsetTop - image.offsetHeight / 2) + 'px';
+    form.style.zIndex = '2';
+
+    form.appendChild(marker);
+    form.appendChild(commentsBody);
+    formContainer.appendChild(form);
+    commentsClose.addEventListener('click', removeEmptyComment);
+    commentsBody.style.display = 'block';
   }
 }
 
