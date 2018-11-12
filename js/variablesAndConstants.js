9ddd3f0b917f74f0a7f74cc28852141aa78cdb4c
likesatie@gmail.com <likesatie@gmail.com>
2018-11-12 20:46:08 +0300

    C_02

--- js/variablesAndConstants.js
+++ js/variablesAndConstants.js
@@ -23,3 +23,16 @@ const comments = document.querySelector('.comments');
 const commentsForm = document.querySelectorAll('.comments__form');
 const commentsOn = document.getElementById('comments-on');
 const commentsOff = document.getElementById('comments-off');
+
+// Server
+
+const error = document.querySelector('.error');
+const imgLoad = document.querySelector('.image-loader');
+const image = document.querySelector('.current-image');
+const errorMessage = document.querySelector('.error__message');
+const url = document.querySelector('.menu__url');
+const share = document.querySelector('.share');
+const copy = document.querySelector('.menu_copy');
+
+let connection;
+let imageId;
